fab5e46135c35fe4e498714de995beaf65732039
Jiangbeibei <1157743680@qq.com>
2021-05-26 16:35:27 +0800

    第一次简单修改

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+//引入完整的element
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
+//createApp(App).use(store).use(router).mount('#app')
